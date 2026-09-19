bc8796cebd87eabfc8d79b72675b798b407012a4
sembunly <sembunly2005@gmail.com>
2026-09-19 18:48:31 +0700

    Connect frontend to Render backend

--- vite.config.js
+++ vite.config.js
@@ -3,40 +3,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-
-  // LOCAL DEVELOPMENT ONLY
-
-  //
-  // React:
-  // http://localhost:5173
-  //
-  // Express:
-  // http://localhost:3000
-  //
-  // Browser calls:
-  // /download
-  //
-  // Vite forwards it to:
-  // http://localhost:3000/download
-
-  server: {
-    proxy: {
-      "/download": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-
-      "/health": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
-
-  // PRODUCTION:
-  // Vite proxy above is NOT used after `npm run build`.
-  //
-  // In production use VITE_API_URL instead:
-  //
-  // VITE_API_URL=https://api.yourdomain.com
 });
